9d5bd9a9376cbfefaf630a0598aa45bcb4263f78
dmjio <code@dmj.io>
2025-06-11 15:21:31 -0500

    Add formatting to example

--- examples/counter/Main.hs
+++ examples/counter/Main.hs
@@ -1,51 +1,45 @@
-{-# LANGUAGE DataKinds #-}
+-----------------------------------------------------------------------------
+{-# LANGUAGE DataKinds          #-}
 {-# LANGUAGE RecordWildCards    #-}
 {-# LANGUAGE OverloadedStrings  #-}
-{-# LANGUAGE DerivingStrategies #-}
-
--- | Haskell module declaration
+-----------------------------------------------------------------------------
 module Main where
-
--- | Miso framework import
-import           Prelude hiding (unlines)
-
+-----------------------------------------------------------------------------
 import           Miso hiding (id_)
 import           Miso.Lens
 import           Miso.String
 import           Miso.Native
 import qualified Miso.Style as CSS
-import qualified Data.Map.Strict as M
-
+-----------------------------------------------------------------------------
+import           Prelude hiding (unlines)
+-----------------------------------------------------------------------------
 -- | Type synonym for an application model
 newtype Model = Model { _value :: Int }
-  deriving stock (Show, Eq)
-
+  deriving (Show, Eq)
+-----------------------------------------------------------------------------
 instance ToMisoString Model where
   toMisoString (Model v) = toMisoString v
-
+-----------------------------------------------------------------------------
 value :: Lens Model Int
 value = lens _value $ \m v -> m { _value = v }
-
--- | Sum type for application events
+-----------------------------------------------------------------------------
 data Action
   = AddOne
   | SubtractOne
   | SayHelloWorld
   | Tap Int
   deriving (Show, Eq)
-
+-----------------------------------------------------------------------------
 -- | Entry point for a miso application
 main :: IO ()
 main = run $ native vcomp
-  { events = M.singleton "tap" False
+  { events = nativeEvents
   , initialAction = Just SayHelloWorld
   }
-
--- | Componentlication definition (uses 'defaultComponent' smart constructor)
+-----------------------------------------------------------------------------
 vcomp :: Component "counter" Model Action
 vcomp = defaultComponent (Model 0) updateModel viewModel
-
--- | UpdateModels model, optionally introduces side effects
+-----------------------------------------------------------------------------
 updateModel
   :: Action
   -> Effect Model Action
@@ -59,13 +53,13 @@ updateModel SubtractOne = do
   value -= 1
 updateModel (Tap x) =
   io_ $ consoleLog ("tapped: " <> ms (show x))
-
+-----------------------------------------------------------------------------
 -- | Constructs a virtual DOM from a model
 viewModel :: Model -> View Action
 viewModel m = view_ []
   [ view_
     [ onTap AddOne
---    , id_ "plus"
+    , id_ "plus"
     , CSS.style_
         [ CSS.backgroundColor CSS.yellow
         , CSS.width "100px"
@@ -83,13 +77,13 @@ viewModel m = view_ []
     ]
     [ text_
       []
-      [ "Miso counter app!"
+      [ "🍜 Miso counter"
       , text ("value: " <> ms m)
       ]
     ]
   , view_
     [ onTap SubtractOne
---    , id_ "minus"
+    , id_ "minus"
     , CSS.style_
         [ CSS.backgroundColor CSS.pink
         , CSS.width "100px"
@@ -100,3 +94,4 @@ viewModel m = view_ []
     [ text_ [ ] [ "-" ]
     ]
   ]
+-----------------------------------------------------------------------------
