5af733941e51868e36d8459c0f5bca406234ccca
dmjio <code@dmj.io>
2025-06-17 17:32:35 -0500

    Add events for `<image>`

--- src/Miso/Native/Element/Image/Event.hs
+++ src/Miso/Native/Element/Image/Event.hs
@@ -11,8 +11,8 @@
 ----------------------------------------------------------------------------
 module Miso.Native.Element.Image.Event
   ( -- *** Events
-    onBindLoad
-  , onBindError
+    onLoad
+  , onError
   -- *** Decoder
   , imageLoadDecoder
   , imageErrorDecoder
@@ -27,33 +27,63 @@ import           Miso.Event
 import           Miso.String (MisoString)
 import           Miso.Types (Attribute)
 -----------------------------------------------------------------------------
--- | <https://lynxjs.org/api/elements/built-in/image.html#bindload>
+-- | https://lynxjs.org/api/elements/built-in/image.html#bindload
 --
-onBindLoad :: (ImageLoadEvent -> action) -> Attribute action
-onBindLoad action = on "bindload" imageLoadDecoder (\e _ -> action e)
+-- Triggered when the image request succeeds, outputting the image's width and height.
+--
+-- @
+--
+-- data Action = HandleImageLoad ImageLoadEvent
+--
+-- view :: Model -> View Action
+-- view model = image_ "url" [ onLoad HandleImageLoad ]
+--
+-- update :: Action -> Effect Model Action
+-- update (HandleImageLoad ImageLoadEvent {..}) = do
+--   io_ (consoleLog "image load event received")
+--
+-- @
+--
+onLoad :: (ImageLoadEvent -> action) -> Attribute action
+onLoad action = on "bindload" imageLoadDecoder (\e _ -> action e)
 -----------------------------------------------------------------------------
--- | <https://lynxjs.org/api/elements/built-in/image.html#binderror>
+-- | https://lynxjs.org/api/elements/built-in/image.html#binderror
 --
-onBindError :: (ImageErrorEvent -> action) -> Attribute action
-onBindError action = on "binderror" imageErrorDecoder (\e _ -> action e)
+-- Triggered when the image request fails, outputting the error message and code.
+--
+-- @
+--
+-- data Action = HandleImageError ImageErrorEvent
+--
+-- view :: Model -> View Action
+-- view model = image_ "url" [ onError HandleImageError ]
+--
+-- update :: Action -> Effect Model Action
+-- update (HandleImageError ImageErrorEvent {..}) = do
+--   io_ (consoleLog "image error event received")
+--
+-- @
+--
+onError :: (ImageErrorEvent -> action) -> Attribute action
+onError action = on "binderror" imageErrorDecoder (\e _ -> action e)
 -----------------------------------------------------------------------------
 -- | Callback when an 'image_' fails to load
 data ImageErrorEvent
   = ImageErrorEvent
-  { errMsg :: MisoString
+  { errorMessage :: MisoString
     -- ^ error message
   , errorCode :: Int
     -- ^ error code
-  , categorizedCode :: Int
+  , lynxCategorizedCode :: Int
     -- ^ lynx specific error code
   } deriving (Show, Eq)
 -----------------------------------------------------------------------------
 -- | Callback when an 'image_' succeeds in loading
 data ImageLoadEvent
   = ImageLoadEvent
-  { width :: Int
+  { imageWidth :: Int
     -- ^ 'image_' width
-  , height :: Int
+  , imageHeight :: Int
     -- ^ 'image_' height
   } deriving (Show, Eq)
 -----------------------------------------------------------------------------
