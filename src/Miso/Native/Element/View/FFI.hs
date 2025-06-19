63b87f62aa5734a0f8e348ff27cbcee82dc20401
dmjio <875324+dmjio@users.noreply.github.com>
2025-06-18 19:01:02 -0500

    Add `getClientBoundingRect` (#29)
    * Add `getClientBoundingRect`
    
    * Use globalThis['invokeExec']

--- src/Miso/Native/Element/View/FFI.hs
+++ src/Miso/Native/Element/View/FFI.hs
@@ -1,5 +1,8 @@
 -----------------------------------------------------------------------------
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications    #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Miso.Native.Element.View.FFI
@@ -14,12 +17,121 @@ module Miso.Native.Element.View.FFI
     boundingClientRect
   , takeScreenshot
   , requestAccessibilityFocus
+  -- *** Types
+  , Rect (..)
+  , BoundingClientRect (..)
+  -- *** Smart constructors
+  , defaultBoundingClientRect
   ) where
 -----------------------------------------------------------------------------
-import           Language.Javascript.JSaddle (JSM)
+import Control.Monad
+import Language.Javascript.JSaddle
 -----------------------------------------------------------------------------
-boundingClientRect :: JSM ()
-boundingClientRect = undefined
+import Miso
+import Miso.String
+-----------------------------------------------------------------------------
+-- | Result of calling `getClientBoundingRect`
+data Rect
+  = Rect
+  { x,y :: Double
+  , width, height :: Double
+  , top, bottom :: Double
+  , right, left :: Double
+  } deriving (Show, Eq)
+-----------------------------------------------------------------------------
+instance FromJSVal Rect where
+  fromJSVal = \rect -> do
+    let readProp = \name ->
+          fromJSValUnchecked =<<
+            rect ! (name :: MisoString)    
+    x      <- readProp "x"
+    y      <- readProp "y"
+    height <- readProp "height"
+    width  <- readProp "width"
+    top    <- readProp "top"
+    right  <- readProp "right"
+    left   <- readProp "left"
+    bottom <- readProp "bottom"
+    pure $ Just Rect {..}
+-----------------------------------------------------------------------------
+data BoundingClientRect action
+  = BoundingClientRect
+  { successful :: Rect -> action
+  -- ^ Successful callback
+  , errorful :: MisoString -> action
+  -- ^ Errorful callback
+  , selector :: MisoString
+  -- ^ Selector identifier (e.g. "#box")
+  , androidEnableTransformProps :: Bool
+  -- ^ Specifies whether to consider the transform attribute
+  -- when calculating the position on Android. The default value is 'False'
+  , relativeTo :: Maybe JSVal
+  -- ^ Specify the reference node, relative to LynxView by default.
+  }
+-----------------------------------------------------------------------------
+-- | Smart constructor for calling 'boundingClientRect'
+defaultBoundingClientRect
+  :: MisoString
+  -- ^ Selector (e.g. "#box")
+  -> (Rect -> action)
+  -- ^ Successful callback (contains 'Rect')
+  -> (MisoString -> action)
+  -- ^ Errorful callback, contains error message
+  -> BoundingClientRect action
+defaultBoundingClientRect selector successful errorful
+  = BoundingClientRect
+  { androidEnableTransformProps = True
+  , relativeTo = Nothing
+  , ..
+  }
+-----------------------------------------------------------------------------
+-- | https://lynxjs.org/api/elements/built-in/view.html#boundingclientrect
+--
+-- The front end can execute 'boundingClientRect' through the SelectorQuery API.
+--
+-- @
+--
+-- data Action
+--   = Success Rect
+--   | Failure MisoString
+--   | GetRect
+--
+-- update :: Action -> Effect model Action
+-- update GetRect = boundingClientRect
+--   (defaultBoundingClientRect "#box" Success Failure)
+-- update (Succes Rect {..}) =
+--   consoleLog "Successfuly got Rect"
+-- update (Failure errorMsg) =
+--   consoleLog ("Failed to call getClientBoundingRect: " <> errorMsg)
+--
+-- @
+--
+boundingClientRect
+  :: BoundingClientRect action
+  -> Effect model action
+boundingClientRect BoundingClientRect {..} = withSink $ \sink -> do
+  selector_ <- toJSVal selector
+  successful_ <- toJSVal =<< do
+    syncCallback1 $ \arg -> do
+      rect <- fromJSValUnchecked arg
+      sink (successful rect)
+  errorful_ <- toJSVal =<< do
+    syncCallback1 $ \arg -> do
+      rect <- fromJSValUnchecked arg
+      sink (errorful rect)
+  params <- create
+  set "androidEnableTransformProps" androidEnableTransformProps params
+  set "relativeTo" relativeTo params
+  params_ <- toJSVal params
+  method__ <- toJSVal ("boundingClientRect" :: MisoString)
+  void $ do
+    jsg ("globalThis" :: MisoString) # ("invokeExec" :: MisoString) $
+      [ selector_
+      , method__
+      , params_
+      , successful_
+      , errorful_
+      ]
 -----------------------------------------------------------------------------
 takeScreenshot :: JSM ()
 takeScreenshot = undefined
