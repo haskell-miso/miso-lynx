656722761703d1be1a0efbbc6c2bfc3c76cd3498
dmjio <875324+dmjio@users.noreply.github.com>
2025-06-21 13:57:53 -0500

    Update `flake.nix` (#35)
    - [x] Use the correct compiler, this ensures cache hits
    - [x] Add additional systems to the `flake.nix`

--- flake.nix
+++ flake.nix
@@ -1,21 +1,26 @@
 {
   description = "A flake for acquiring GHCJS 9.12.2 modded w/ the JSBI.patch";
+
   inputs = {
+
     nixpkgs.url =
       "https://github.com/alexfmpe/nixpkgs/archive/b594b289740a2bc917ed9c66fef5d905f389cb96.tar.gz";
+
+    flake-utils.url =
+      "github:numtide/flake-utils";
+
   };
-  outputs = { self, nixpkgs }: {
-    nixConfig = {
-      extra-substituters = [
-        "https://haskell-miso-cachix.cachix.org"
-      ];
-      extra-trusted-public-keys = [
-        "haskell-miso-cachix.cachix.org-1:m8hN1cvFMJtYib4tj+06xkKt5ABMSGfe8W7s40x1kQ0="
-      ];
-    };
-    devShells.x86_64-linux.default =
+  outputs = { self, nixpkgs, flake-utils }:
+
+   flake-utils.lib.eachSystem
+      [
+        "x86_64-linux"
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+      ] (system:
+
       let
-        system = "x86_64-linux";
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ (import ./nix/overlay.nix) ];
@@ -24,12 +29,23 @@
             allowBroken = false;
           };
         };
-      in
-        pkgs.mkShell {
-          buildInputs = with pkgs; [
-            pkgs.haskell.packages.ghcNative.miso-native.env
-            cabal-install
+
+      in {
+        nixConfig = {
+          extra-substituters = [
+            "https://haskell-miso-cachix.cachix.org"
+          ];
+          extra-trusted-public-keys = [
+            "haskell-miso-cachix.cachix.org-1:m8hN1cvFMJtYib4tj+06xkKt5ABMSGfe8W7s40x1kQ0="
           ];
         };
-  };
+        devShells.default =
+          pkgs.mkShell {
+            name = "miso-native shell";
+            buildInputs = with pkgs; [
+              pkgs.pkgsCross.ghcjs.haskell.packages.ghcNative.ghc
+            ];
+          };
+      }
+   );
 }
