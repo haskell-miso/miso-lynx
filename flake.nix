75b2920b97ed48091fab10bb1f1a2e990f685853
dmjio <code@dmj.io>
2025-06-21 00:29:59 -0500

    Add substituters, put miso's cachix cache in there

--- flake.nix
+++ flake.nix
@@ -1,10 +1,18 @@
 {
-  description = "A flake for acquiring GHCJS 9.12.2 w/ modded w/ the JSBI.patch";
+  description = "A flake for acquiring GHCJS 9.12.2 modded w/ the JSBI.patch";
   inputs = {
     nixpkgs.url =
       "https://github.com/alexfmpe/nixpkgs/archive/b594b289740a2bc917ed9c66fef5d905f389cb96.tar.gz";
   };
   outputs = { self, nixpkgs }: {
+    nixConfig = {
+      extra-substituters = [
+        "https://haskell-miso-cachix.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "haskell-miso-cachix.cachix.org-1:m8hN1cvFMJtYib4tj+06xkKt5ABMSGfe8W7s40x1kQ0="
+      ];
+    };
     devShells.x86_64-linux.default =
       let
         system = "x86_64-linux";
@@ -19,7 +27,8 @@
       in
         pkgs.mkShell {
           buildInputs = with pkgs; [
-            ghc-native
+            pkgs.haskell.packages.ghcNative.miso-native.env
+            cabal-install
           ];
         };
   };
