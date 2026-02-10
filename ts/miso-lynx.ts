4ce7c30500e5dcc7caf4342886aaed753a27bf9d
dmjio <code@dmj.io>
2026-02-10 07:59:07 -0600

    Use `__MAIN_THREAD__`

--- ts/miso-lynx.ts
+++ ts/miso-lynx.ts
@@ -19,14 +19,7 @@ globalThis['TextEncoder'] = TextEncoder;
 globalThis['BigInt'] = JSBI.BigInt;
 globalThis['JSBI'] = JSBI;
 
-function onBTS () : boolean {
-  if (!('__CreatePage' in globalThis)) {
-    return true;
-  };
-  return false;
-}
-
-if (onBTS()) {
+if (!(__MAIN_THREAD__)) {
   globalThis['native'] = {
     drawingContext : btsContext.drawingContext,
     eventContext : btsContext.eventContext
